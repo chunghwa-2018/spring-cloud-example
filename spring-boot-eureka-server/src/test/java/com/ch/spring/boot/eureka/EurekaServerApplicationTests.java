34ba405c8c3c4b1f41cdd6b86152d7f766f48784
zhaohongliang <15711385916@163.com>
2022-07-17 11:27:31 +0800

    add module spring-boot-eureka-server

---
+++ spring-boot-eureka-server/src/test/java/com/ch/spring/boot/eureka/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ch.spring.boot.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
