1d38abac49cacee852cd6c2601192b5a01075819
zhaohongliang <15711385916@163.com>
2022-07-17 11:28:02 +0800

    add module spring-boot-eureka-client

---
+++ spring-boot-eureka-client/src/main/java/com/ch/spring/boot/eureka/controller/StudentsController.java
@@ -0,0 +1,36 @@
+package com.ch.spring.boot.eureka.controller;
+
+import com.netflix.discovery.DiscoveryClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * <p>学生 controller </p>
+ *
+ * @ClassName StudentsController
+ * @Description 学生 controller
+ * @Author zhaohongliang
+ * @Date 2022-07-16 20:45
+ * @Version 1.0
+ */
+@RestController
+@RequestMapping("/student")
+public class StudentsController {
+
+    /**
+     * 日志
+     */
+    private static final Logger logger = LoggerFactory.getLogger(StudentsController.class);
+
+    //@Autowired
+    //private DiscoveryClient discoveryClient;
+
+    @RequestMapping("/eureka")
+    public String index() {
+        return "hello eureka";
+    }
+}
