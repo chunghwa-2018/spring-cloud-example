cd6dca7f027986a30c5255770bb0dda128070d80
zhaohongliang <15711385916@163.com>
2022-07-19 01:31:29 +0800

    解决 hystrix-dashboard Unable to connect to Command Metric Stream. bug

--- spring-boot-hystrix-dashboard/src/main/java/com/ch/spring/boot/hystrix/HystrixDashboardApplication.java
+++ spring-boot-hystrix-dashboard/src/main/java/com/ch/spring/boot/hystrix/HystrixDashboardApplication.java
@@ -1,8 +1,12 @@
 package com.ch.spring.boot.hystrix;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.Bean;
 
 /**
  * 浏览器F12，console出现 Uncaught: TypeError: e.indexOf is not a function.错误
@@ -10,11 +14,20 @@ import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboar
  * 修改 monitor.ftlh 为如下
  * $(window).load(function() {
  */
+@EnableHystrix
 @EnableHystrixDashboard
 @SpringBootApplication
 public class HystrixDashboardApplication {
 
-
+	@Bean
+	public ServletRegistrationBean getServlet() {
+		HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+		ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+		registrationBean.setLoadOnStartup(1);
+		registrationBean.addUrlMappings("/hystrix.stream");
+		registrationBean.setName("HystrixMetricsStreamServlet");
+		return registrationBean;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HystrixDashboardApplication.class, args);
