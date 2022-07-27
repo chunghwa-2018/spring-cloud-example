11ef4266e8b5747af5e450912c7a9807ec0305f0
zhaohongliang <15711385916@163.com>
2022-07-28 03:32:35 +0800

    @EnableDiscoveryClient 当前版本约定大于配置，该注解已激活

--- spring-cloud-eureka-provider/src/main/java/com/ch/spring/boot/eureka/EurekaProviderApplication.java
+++ spring-cloud-eureka-provider/src/main/java/com/ch/spring/boot/eureka/EurekaProviderApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
+// 当前版本约定大于配置，该注解已激活
+// @EnableDiscoveryClient
 @SpringBootApplication
 public class EurekaProviderApplication {
 
