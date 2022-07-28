a2e6dd946e41056daa02e7c96dc14a388899a047
zhaohongliang <15711385916@163.com>
2022-07-29 01:26:16 +0800

    优雅停服 shell 脚本优化

--- spring-cloud-shutdown-server.sh
+++ spring-cloud-shutdown-server.sh
@@ -2,12 +2,11 @@
 # author: chunghwa
 # github: https://github.com/chunghwa-2018/
 
-# server_array=('8081', '8082', '8083', '8084', '8085', '8086')
+server_array=('8081' '8082' '8083' '8084' '8085' '8086')
 
-http post http://localhost:8081/actuator/shutdown
-http post http://localhost:8082/actuator/shutdown
-http post http://localhost:8083/actuator/shutdown
-http post http://localhost:8084/actuator/shutdown
-http post http://localhost:8005/actuator/shutdown
-http post http://localhost:8006/actuator/shutdown
+for loop in "${server_array[@]}"
+do
+    http post http://localhost:$loop/actuator/shutdown
+done
+
 
