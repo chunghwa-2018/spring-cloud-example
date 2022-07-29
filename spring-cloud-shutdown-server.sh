05bf4551ba92adc75414379fcbfaad1c0ae74cfb
zhaohongliang <15711385916@163.com>
2022-07-29 22:59:41 +0800

    优雅停服 shell 脚本

--- spring-cloud-shutdown-server.sh
+++ spring-cloud-shutdown-server.sh
@@ -3,10 +3,24 @@
 # github: https://github.com/chunghwa-2018/
 
 server_array=('8081' '8082' '8083' '8084' '8085' '8086')
-
+ 
 for loop in "${server_array[@]}"
 do
     http post http://localhost:$loop/actuator/shutdown
 done
 
+
+# APP_PID_TEMP=$(jps -l | grep spring-cloud-)
+# eval "$(echo "$APP_PID_TEMP" | awk '{printf("pid=%s;pidname=%s",$1,$2)}')"
+# echo "The load project is $pidname, pid is:$pid"
+# APP_ID=$pid
+# echo $APP_ID
+
+
+for k in `jps -l | grep spring | awk '{print($1,$2)}'`
+do
+    echo $k
+done
+
+# APP_PID_TEMP2=`jps -l | grep spring | awk '{ print}'`
 
